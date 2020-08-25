2a1716969900eb25ec3a6134cc2afa4287365136
Ignacio Pérez Cumillaf <iperezc99@gmail.com>
2020-08-25 15:58:33 -0400

    created a node server to fully handle stripe requests

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
-
+import axios from 'axios';
 
 const StripeCheckoutButton = ({price}) => {
     const priceForStripe = price*100; //cent into dollar
     const publishableKey = 'pk_test_51H0vPFLqCJvAfUDEnE7svCACf2I3E3E8l1CvFMa9NCHtff7LFxVJGqnw2FbCCqIwM2TTQGsjt1m8dk9FBZh4SCSc00hxWh9rMr';
     const onToken = token => {
-        console.log(token);
-    }
+        axios({
+          url: 'payment',
+          method: 'POST',
+          data: {
+            amount: priceForStripe,
+            token
+          }
+        })
+          .then(response => {
+            alert('succesful payment');
+          })
+          .catch(error => {
+            console.log('Payment Error: ', error);
+            alert(
+              'There was an issue with your payment! Please make sure you use the provided credit card.'
+            );
+          });
+      };
 
     return(
         <StripeCheckout
@@ -25,5 +41,4 @@ const StripeCheckoutButton = ({price}) => {
     );
 
 }
-
 export default StripeCheckoutButton;
